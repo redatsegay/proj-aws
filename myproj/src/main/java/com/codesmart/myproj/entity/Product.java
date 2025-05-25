5ac440e23507ad1f45b1d48d1f5cfce64d931b29
Gebre Reda <gebre15tsegay@gmail.com>
2025-05-25 16:59:07 -0500

    commiting

---
+++ myproj/src/main/java/com/codesmart/myproj/entity/Product.java
@@ -0,0 +1,66 @@
+package com.codesmart.myproj.entity;
+
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product order = (Product) o;
+        return Objects.equals(getId(), order.getId()) && Objects.equals(getName(), order.getName()) && Objects.equals(getDescription(), order.getDescription()) && Objects.equals(getStatus(), order.getStatus()) && Objects.equals(getDate(), order.getDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDescription());
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ myproj/src/main/java/com/codesmart/myproj/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.codesmart.myproj.service;
+
+
+import com.codesmart.myproj.entity.Product;
+import com.sun.jdi.LongValue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductService {
+
+    List<Product> productList = new ArrayList<>(
+            Arrays.asList(
+                    new Product(1L,"Grape","This is a very nice grape"),
+                    new Product(2L,"Banana","This is a very nice Banana"),
+                    new Product(3L,"Orange","This is a very nice Orange")
+
+            ));
+
+    //return a product by id
+    public Product getProduct(Long id){
+        return productList.get(id.intValue());
+    }
+
+    public List<Product> getProductList(){
+        return productList;
+    }
+}
